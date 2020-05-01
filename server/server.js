d5d5089bf88a635e415040ee60665c4916524edb
jonnyps <jonny_spry@msn.com>
2020-05-01 23:18:25 +0100

    reconfigure frontend to access backend

--- server/server.js
+++ server/server.js
@@ -4,13 +4,11 @@ var cors = require('cors')
 var request = require('request');
 var apikey = require('./api-key');
 
-// var secret = apikey.key;
-// console.log(secret)
+// use cors to get around any cors errors
 app.use(cors())
 
-
+// make a request to a third party api and send the data to our front end
 app.get('/', function (req, res) {
-
   request("https://api.weatherapi.com/v1/forecast.json?key="+apikey.key+"&q=London&days=5", function (error, response, body) {
     if (!error && response.statusCode == 200) {
       res.send(body)
@@ -21,6 +19,5 @@ app.get('/', function (req, res) {
 var server = app.listen(8081, function () {
    var host = server.address().address
    var port = server.address().port
-   
    console.log("Example app listening at http://%s:%s", host, port)
 })
--- src/App.js
+++ src/App.js
@@ -12,63 +12,46 @@ class App extends React.Component {
     this.state = {
       data: []
     }
-    // this.getData=this.getData.bind(this);
+    this.getData=this.getData.bind(this);
   }
 
   componentDidMount() {
     console.log('component has mounted!!')
-    // this.getData();
-    let request = new XMLHttpRequest();
-    request.open('GET', 'http://localhost:8081');
-    // request.setRequestHeader('Access-Control-Allow-Origin', '*');
-    request.onload = function() {
-      let response = request.response;
-      console.log(response)
-      // let parsedData = JSON.parse(response);
-      // console.log(parsedData)
-         
-    }
-    request.send();
-
+    this.getData();
   }
 
   getData() {
+    // get data from our backend
     let request = new XMLHttpRequest();
-
-    request.open('GET', 'https://api.weatherapi.com/v1/forecast.json?key=196782bbf2cd492385e161918200105&q=London&days=5');
+    request.open('GET', 'http://localhost:8081');
     request.onload = function() {
       let response = request.response;
       let parsedData = JSON.parse(response);
-      console.log(parsedData)
       this.setState({
         data: parsedData
       }) 
-      console.log(this.state.data.location.name)      
     }.bind(this)
     request.send();
   }
 
   render() {
-    return (
-      <p>hello world</p>
-    )
-      // if (this.state.data.length === 0) {
-      //   return (
-      //   // Render loading state ...
-      //   <p>loading...</p>
-      //   )
-      // } else {
-      //   return (
-      //   // Render real UI ...
-      //   <div className="app">
-      //     <Title text={this.state.data.location.name} />
-      //     <Current currentWeather={this.state.data.current} />
-      //     <Forecast forecast={this.state.data.forecast.forecastday} />
-      //     <Updated text={this.state.data.current.last_updated} />
-      //   </div>
-      //   )
-      //   // console.log(this.state.data.current.last_updated)
-      // }
+      if (this.state.data.length === 0) {
+        return (
+        // Render loading state ...
+        <p>loading...</p>
+        )
+      } else {
+        return (
+        // Render real UI ...
+        <div className="app">
+          <Title text={this.state.data.location.name} />
+          <Current currentWeather={this.state.data.current} />
+          <Forecast forecast={this.state.data.forecast.forecastday} />
+          <Updated text={this.state.data.current.last_updated} />
+        </div>
+        )
+        // console.log(this.state.data.current.last_updated)
+      }
     }
 }
 
